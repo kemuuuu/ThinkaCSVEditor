d80f546ef487d6fbc8d8833e2bdbca6923708679
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-10-25 12:01:58 +0900

    Modify jsonGenerator csvGenerator

--- src/csvGenerator.js
+++ src/csvGenerator.js
@@ -7,7 +7,7 @@ class CsvGenerator {
   }
 
   generate() {
-    this._json.reduce((acc, cur, i, arr) => {
+    this._json.map((cur, i, arr) => {
       const line = [];
       Object.keys(cur).map(key => {
         // 処理中のJSONに_headerが持たないキーがあれば、最後尾に加える
@@ -15,12 +15,24 @@ class CsvGenerator {
         // csv行の作成
         line[this._header.indexOf(key)] = cur[key];
       });
+      // undefinedは空のStringに置き換える
       const filled = line.map(e => {
         if (!e) return String('');
         return e;
-      })
-      this._body.push(filled.join());
+      });
+      this._body.push(filled);
     });
+
+    // 全行の処理が完了したらヘッダの列数を確認し、ボディの不足分を埋める
+    this._body = this._body.map((line) => {
+      const diff = this._header.length - line.length;
+      if (diff > 0) {
+        const fil = new Array(diff).fill(String(''));
+        return line.concat(fil).join();
+      }
+      return line.join();
+    });
+
     const csv = this._header.join() + '\n' + this._body.join('\n');
     return csv;
   }
--- src/jsonGenerator.js
+++ src/jsonGenerator.js
@@ -8,9 +8,10 @@ class JsonGenerator {
 
   generate() {
     // 1行を1要素としたリストを作成
-    this._data.split('\n').reduce((acc,line,i) => {
-      // 1行目はヘッダなのでスキップ
-      if (i < 1) return;
+    this._data.split('\n').map((line,lineNumber) => {
+
+      // 1行目はヘッダーなのでスキップ
+      if (lineNumber < 1) return;
 
       // 1行ごとのデータを格納するJSON
       let json_tmp = {}
@@ -19,7 +20,8 @@ class JsonGenerator {
       const delimitered = line.split(',').map((cell) => cell.replace(/"/g,''));
 
       // 1セルずつJSONに加えていく
-      delimitered.reduce((acc, cur, i, arr) => {
+      delimitered.map((cur, i, arr) => {
+        // フラグがtrueであれば、すでに現在の列はjsonに加えられているのでスキップ
         if (this._flg) {
           this._flg = false;
           return;
@@ -33,9 +35,10 @@ class JsonGenerator {
           if (i == 4) json_tmp['性別']= cur;
           if (i == 5) json_tmp['誕生日'] = cur;
           if (i == 6) json_tmp['顧客状況'] = cur;
-          // カラム7以降は自由項目
-          if (i > 6) {
+          // カラム8以降は自由項目
+          if (i >= 7) {
             // 奇数列はkey
+            // 次の列をvalueに入れてフラグを立てる
             if (i%2 !== 0) {
               json_tmp[cur] = arr[i+1];
               this._flg = true;
